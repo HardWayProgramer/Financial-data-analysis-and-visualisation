975a9a16d4cdd9713a2c5b5d5c38a7393e1c21ca
Pedro Salamanca <pioq304e@op.pl>
2017-02-18 16:15:17 +0100

    Window and button

---
+++ UserIfc.py
@@ -0,0 +1,30 @@
+import sys
+from PyQt4 import QtGui, QtCore
+
+class Window(QtGui.QMainWindow):
+  def __init__(self,appref):
+    super(Window,self).__init__()
+    scrRes = appref.desktop().screenGeometry()
+    self.setGeometry(1,25,scrRes.width(),(scrRes.height()-53))
+    self.setWindowTitle("Notowania gieldowe")
+    self.initComponents()
+  def initComponents(self):
+    btn = QtGui.QPushButton("Exit",self)
+    QtCore.QObject.connect(btn,QtCore.SIGNAL('clicked()'),QtCore.QCoreApplication.instance().quit)
+    btn.resize(100,50)
+    btn.move(100,100)
+    self.show()
+
+def main():
+  app = QtGui.QApplication(sys.argv)
+  GUI = Window(app)
+  sys.exit(app.exec_())
+
+
+
+
+
+
+
+
+main()
