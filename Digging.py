ccc7e1ecb79e2af13348c94a87baa3343a30ec6c
PiotrSzeluga <piotr_szeluga@op.pl>
2017-03-08 11:32:38 +0100

    adding database modifying Digging.py adding backend functions: initCurrList, createCompanyTable, createCurrencyTable,fromDatabase,toDatabase,scrapYahoo,scrapNBP,__init__

--- Digging.py
+++ Digging.py
@@ -1,10 +1,66 @@
+#!/usr/bin/python
+#! -*- coding: utf-8 -*-
+from threading import Thread
+import sqlite3
+import urllib2
+
 class Digger():
+  stockList = ['DJI','FTSE100']
+  currList = []
   DJI = ['PFE','V','UTX','KO','TRV','MSFT','INTC','MRK','MMM','AXP','GE','AAPL','CVX','PG','JPM','GS','DD','CSCO','IBM','DIS','CAT','XOM','JNJ','WMT','NKE','MCD','HD','BA','VZ','UNH']
-  FTSE100 = ['EXPN.L','AHT.L','SKY.L','CPG.L','PFG.L','WPG.L','PPB.L','VOD.L','SDR.L','BATS.L','PSN.L','SSE.L','GKN.L']
-  NIKKEI225 = []
+  FTSE100 = ['EXPN.L','AHT.L','SKY.L','CPG.L','PFG.L','WPG.L','PPB.L','VOD.L','SDR.L','BATS.L','PSN.L','SSE.L','GKN.L','RB.L','BA.L','TUI.L','PRU.L','RDSA.L','RR.L','CCL.L','MDC.L','STJ.L','TSCO.L','CCH.L','EZJ.L','CNA.L','SHP.L','ANTO.L','DLG.L']
+  def __init__(self):
+    self.connection = None
+    try:
+      self.connection = sqlite3.connect('companiesxcurrencies.db')
+      self.cur = self.connection.cursor()
+      print 'connected'
+      self.cur.execute("SELECT name FROM sqlite_master WHERE type='table';")
+      if( self.cur.fetchone() == None ):
+        print 'empty db'
+        self.initCurrList()
+        #self.createCompanyTable(lambda x,y:x+y,self.DJI,self.FTSE100)
+        #self.createCurrencyTable()
+
+    except sqlite3.Error,e:
+      print 'error :{}'.format(e.args[0])
+      
+  def initCurrList(self):
+    src = urllib2.urlopen('http://www.nbp.pl/kursy/Archiwum/archiwum_tab_a_2012.csv').read()
+    lines = src.split('\n')
+    items = []
+    for line in lines:
+      items.append(line.split(';'))
+    print len(items[0])
+    self.currList = items[0][1:38]
+
+  def createCompanyTable(self,com):
+    try:
+      for i in com:
+        self.cur.execute('''CREATE TABLE ? (ID TEXT PRIMARY KEY  NOT NULL,CLOSEP REAL,MAXP REAL,MINP REAL,OPENP REAL,VOL REAL);''',(i))	
+    except sqlite3.Error,e:
+      print 'error:{}'.format(e.args[0])
+
+  def createCurrencyTable(self):
+    try:
+      for i in self.currList:
+        self.cur.execute('''CREATE TABLE ? (ID TEXT PRIMARY KEY  NOT NULL,VALUE REAL)''',(i))
+    except sqlite3.Error,e:
+      print 'error:{}'.format(e.args[0])
+
+  def fromDatabase(self,tName):
+    print 'f'
+  def toDatabase(self,tName):
+    print 't'
+  def scrapYahoo(self,urlPart):
+    print 'y'
+
+  def scrapNBP(self):
+    print 'n'
 
 def debug():
-  print 'debug runs'
+    x = Digger()
 
+    
 if __name__=='__main__':
   debug()
--- UserIfc.py
+++ UserIfc.py
@@ -1,5 +1,7 @@
+#!/usr/bin/python
+#! -*- coding: utf-8 -*-
 import sys
-import Digging
+from Digging import Digger
 from PyQt4 import QtGui, QtCore
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg as fcanvas
@@ -8,7 +10,7 @@ import matplotlib.dates as mdate
 import time as ct
 import numpy as np
 import urllib2
-from threading import Thread
+
 
 class Window(QtGui.QTabWidget):
   plotX = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]
@@ -30,7 +32,21 @@ class Window(QtGui.QTabWidget):
     errinfo.setGeometry((self.scrRes.width()/2 -150),(self.scrRes.height()/2 -100),300, 200)
     errinfo.setStandardButtons(QtGui.QMessageBox.Ok)
     errinfo.exec_()
-
+  @QtCore.pyqtSlot(str)
+  def on_indexCombo_currentIndexChanged(self,index):
+    companyItems = self.stockItems[str(index)]
+    self.companyCombo.clear()
+    self.companyCombo.addItem('Select Company')
+    self.companyCombo.addItems(companyItems)
+    if self.indexCombo.itemText(0) == 'Select Index':
+      self.indexCombo.removeItem(0)
+      self.companyCombo.currentIndexChanged[str].connect(self.on_companyCombo_currentIndexChanged)
+      self.layout1.addWidget(self.companyCombo,1,10,1,2)
+  @QtCore.pyqtSlot(str)
+  def on_companyCombo_currentIndexChanged(self,index):
+    if self.companyCombo.itemText(0) == 'Select Company':
+      self.companyCombo.removeItem(0)
+    Digger.scrapYahoo(str(index))
 
   def __init__(self,appref):
     super(Window,self).__init__()
@@ -44,20 +60,57 @@ class Window(QtGui.QTabWidget):
     self.layout1 = QtGui.QGridLayout()
     self.layout2 = QtGui.QGridLayout()
     self.tab1.setLayout(self.layout1)
-    self.tab2.setLayout(self.layout2)
-    
+    self.tab2.setLayout(self.layout2) 
+    self.stockItems = {Digger.stockList[0]:Digger.DJI,Digger.stockList[1]:Digger.FTSE100} # edit this line as well as Digging.py in case of adding more companies / indexes
+    self.indexCombo = QtGui.QComboBox(self)
+    self.indexCombo.addItem('Select Index')
+    self.indexCombo.addItems(self.stockItems.keys())
+    self.indexCombo.currentIndexChanged[str].connect(self.on_indexCombo_currentIndexChanged)
+    self.companyCombo = QtGui.QComboBox(self)
+    print self.scrRes.height()
+    self.createPlot()
+    self.pinToLayouts()
     self.tabs.show()
 
-#  def updatePlot():
-#    ax = self.figure.add_subplot(111)
-#    ax.plot(self.plotX,self.plotY)
-#    self.canvas.draw()
+  def pinToLayouts(self):
+    self.layout1.addWidget(self.canvas,0,0,10,10)
+    self.layout1.addWidget(self.toolbar,11,0,1,10)
+    self.layout1.addWidget(self.indexCombo,0,10,1,2)
+    
+    self.layout2.addWidget(self.canvas2,0,0,10,10)
+    self.layout2.addWidget(self.toolbar2,11,0,1,10)
+
+
+  def updatePlot(self,pType,tabId):
+    if tabId == 1:
+      self.wykres1.cla()
+      self.wykres2.cla()
+      self.wykres1.plot(self.plotX,self.plotY,'.k')
+      self.wykres2.plot(self.plotY,self.plotX,'{}'.format(pType))
+      self.canvas.draw()
+    else:
+      self.wykres21.cla()
+      self.wykres22.cla()
+      self.wykres11.plot(self.plotX,self.plotY,'.k')
+      self.wykres22.plot(self.plotY,self.plotX,'{}'.format(pType))
+      self.canvas.draw()
+  
+  def createPlot(self):
+    self.figure = plt.figure()
+    self.figure2 = plt.figure()
+    self.canvas = fcanvas(self.figure)
+    self.canvas2 = fcanvas(self.figure2)
+    self.toolbar = ntbar(self.canvas,self)
+    self.toolbar2 = ntbar(self.canvas2,self)
+    self.wykres1 = self.figure.add_subplot(2,1,1)
+    self.wykres2 = self.figure.add_subplot(2,1,2)
+    self.wykres11 = self.figure2.add_subplot(2,1,1)
+    self.wykres22 = self.figure2.add_subplot(2,1,2)
+    self.wykres1.hold(True)
+    self.wykres11.hold(True)
+
 
 #  def splitToPlot([]):
-
-
-
-
 
 
 def main():
